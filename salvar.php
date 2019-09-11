ebfbbca199b7575c1ca7b24b661f1fd2b10ea2e3
Iury Coelho <38250160+iuryeng@users.noreply.github.com>
2019-09-11 16:52:04 -0300

    Update salvar.php

--- salvar.php
+++ salvar.php
@@ -1,16 +1,11 @@
-
-
 <?php
 include"conexao.php";
-
   
-    $sensor1 = $_GET['sensor1'];
+        $sensor1 = $_GET['sensor1'];
 	$sensor2 = $_GET['sensor2'];
 	$sensor3 = $_GET['sensor3']; 
 
-
 	$sql = "INSERT INTO tbdados (sensor1, sensor2, sensor3) VALUES (:sensor1, :sensor2, :sensor3)";
-
 	$stmt = $PDO->prepare($sql);
 
 	$stmt->bindParam(':sensor1', $sensor1);
@@ -21,13 +16,10 @@ include"conexao.php";
 		{
 		echo "salvo_com_sucesso";
 		}
-
 	else 
 		{
-			echo "erro_ao_salvar";
-
+		echo "erro_ao_salvar";
 		}
-
 ?>
 
 
